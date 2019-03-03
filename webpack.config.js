88fb599db96ccfb1e9f0f6651ad8da6f80d36aa9
Kyle <kyle@kylemacquarrie.co.uk>
2019-03-03 21:31:18 +0000

    Use single webpack config with env variables.

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+const title = 'Chapter 3: The wrong way to draw a line'
+const slug = 'wrong-way-to-draw-a-line'
+const path = require('path').resolve(__dirname)
+const config = require('@velvetkevorkian/sketch-boilerplate-p5').config
+
+module.exports = env => config(title, slug, path, env)
--- webpack.production.js
+++
@@ -1,7 +0,0 @@
-const resolve = require('path').resolve
-const config = require('@velvetkevorkian/sketch-boilerplate-p5').config
-const title = 'Chapter 3: The wrong way to draw a line'
-const slug = 'wrong-way-to-draw-a-line'
-const path = resolve(__dirname)
-
-module.exports = config(title, slug, path, 'production')
